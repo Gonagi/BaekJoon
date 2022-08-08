4b8729db26267a6d2c8e9928831e23f319596414
Gonagi <kkh10002@naver.com>
2022-08-08 22:54:44 +0900

    08.08

--- BaekJoon/Math_1.cpp
+++ BaekJoon/Math_1.cpp
@@ -89,6 +89,7 @@ int main() {
 }
 */
 
+/*
 // 10250번 문제
 
 #include <iostream>
@@ -108,5 +109,168 @@ int main() {
 
 		std::cout << result << std::endl;
 	}
+	return 0;
+}
+*/
+
+/*
+// 2775번 문제
+
+#include <iostream>
+#define Max 100
+int apart[Max][14] = { 0 };
+
+int main() {
+	int T, k, n;
+	for (int i = 0; i < 14; i++)
+		apart[0][i] = i + 1;
+
+	std::cin >> T;
+
+	for (int a = 0; a < T; a++) {
+		std::cin >> k >> n;
+		for (int i = 1; i <= k; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j == 0)
+					apart[i][j] = 1;
+				else
+					apart[i][j] = apart[i - 1][j] + apart[i][j - 1];
+			}
+		}
+		std::cout << apart[k][n-1] << "\n";
+			
+	}
+	return 0;
+}
+
+int getNum(int a, int b){
+	if(b == 1)
+		return 1;
+	if(a == 0)
+		return b;
+	return getNum((a-1, b) + getNum(a, b-1));
+}
+
+int main(){
+	int T, k, n;
+	std::cin >> T;
+	for(int i = 0; i < T; i++){
+		cin >> k >> n;
+		std::cout << getNum(k, n) <<"\n";
+	}
+}
+*/
+
+/*
+// 2839번 문제
+
+#include <iostream>
+
+int main() {
+	int N, result = 0;
+
+	try {
+		std::cin >> N;
+		if (N < 3 || N > 100) {
+			throw std::out_of_range("'N'이 범위를 벗어났습니다.");
+		}
+
+		for (int a = 0; a < N / 3; a++) {
+			if ((N - 3 * a) % 5 == 0) {
+				if (result == 0)
+					result = a + (N - 3 * a) / 5;
+				else if (result > a + (N - 3 * a) / 5)
+					result = a + (N - 3 * a) / 5;
+			}
+		}
+
+		if (result == 0 && N % 3 == 0)
+			result = N / 3;
+		
+		if(result == 0)
+			result = -1;
+
+		std::cout << result;
+
+	}
+	catch (std::out_of_range& exception) {
+		std::cerr << "오류 발생 : " << exception.what();
+	}
+
+	return 0;
+}
+
+#include <iostream>
+int main() {
+	int kg, a, b;
+	std::cin >> kg;
+
+	a = kg / 5;
+	while (1) {
+		if (a < 0) {
+			std::cout << -1;
+			return 0;
+		}
+		if ((kg - (5 * a) % 3 == 0)) {
+			b = (kg - (5 * 1)) / 3;
+			break;
+		}
+		a--;
+	}
+	std::cout << a + b;
+	return 0;
+}
+*/
+
+// 10757번 문제
+#include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+
+int main() {
+	std::string s1, s2;
+	std::vector<int> result;
+	int s1_index, s2_index, cal, upper = 0;
+
+	std::cin >> s1 >> s2;
+	std::strlen(s1);
+	s1_index = static_cast<int>(s1.size())-1;
+	s2_index = static_cast<int>(s2.size())-1;
+
+	while (s1_index != -1 || s2_index != -1) {
+		cal = (s1[s1_index] - '0') + (s2[s2_index] - '0');
+		
+		if(upper == 1){
+			if (cal >= 10) {
+				result.push_back(cal % 10 + 1);
+				upper = 1;
+			}
+			else {
+				result.push_back(cal + 1);
+				upper = 0;
+			}
+		}
+
+		else {
+			if (cal >= 10) {
+				result.push_back(cal % 10);
+				upper = 1;
+			}
+			else {
+				result.push_back(cal);
+				upper = 0;
+			}
+		}
+		
+		s1_index--;
+		s2_index--;
+
+		if (upper == 1 && (s1_index == -1 || s2_index == -1))
+			result.push_back(1);
+	}
+
+	for (int i = static_cast<int>(result.size())-1; i >= 0; i--)
+		std::cout << result.at(i);
 	return 0;
 }
