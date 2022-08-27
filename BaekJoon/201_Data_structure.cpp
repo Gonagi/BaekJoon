f0caf205905ab0854910afa64b87bcf00a5cbfcc
Gonagi <kkh10002@naver.com>
2022-08-27 19:09:32 +0900

    Stack 메모리 누수 확인

--- BaekJoon/201_Data_structure.cpp
+++ BaekJoon/201_Data_structure.cpp
@@ -87,3 +87,124 @@ int main() {
 	return 0;
 }
 */
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <memory>
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+	#ifndef DBG_NEW
+		#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+		#define new DBG_NEW
+	#endif
+#endif
+
+struct Node {
+	int data;
+	Node* next;
+};
+
+struct Stack {
+	Node* top;
+};
+
+class STACK {
+private:
+	struct Stack* stack;
+	int check;
+public:
+	STACK() {
+		stack = new Stack;
+		stack->top = nullptr;
+		check = 0;
+	}
+	~STACK() {
+		std::cout << "¼Ò¸êÀÚ ÀÛµ¿" << "\n";
+		delete stack;
+	}
+	Node* create_node();
+	void push(int data);
+	void pop();
+	void size();
+	void empty();
+	void top();
+};
+
+Node* STACK::create_node() {
+	Node* new_node = new Node;
+	new_node->data = 0;
+	new_node->next = nullptr;
+	return new_node;
+}
+
+void STACK::push(int _data) {
+	Node* new_node = create_node();
+	new_node->data = _data;
+	if (stack->top == nullptr)
+		stack->top = new_node;
+	else {
+		new_node->next = stack->top;
+		stack->top = new_node;
+	}
+	check++;
+}
+void STACK::pop() {
+	if (stack->top == nullptr)
+		std::cout << -1 << "\n";
+	else {
+		std::cout << stack->top->data << "\n";
+		struct Node* old_node = stack->top;
+		stack->top = old_node->next;
+
+		delete old_node;
+		old_node = nullptr;
+		check--;
+	}
+}
+void STACK::size() {
+	std::cout << check << "\n";
+}
+void STACK::empty() {
+	if (stack->top == nullptr)
+		std::cout << 1 << "\n";
+	else
+		std::cout << 0 << "\n";
+}
+void STACK::top() {
+	if (stack->top == nullptr)
+		std::cout << -1 << "\n";
+	else
+		std::cout << stack->top->data << "\n";
+}
+int main() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	// _CrtSetBreakAlloc(255);
+
+	int N, num;
+	std::string str;
+	auto stack = std::make_unique<STACK>();
+
+	std::cin >> N;
+	for (int i = 0; i < N; i++) {
+		std::cin >> str;
+		if (str == "pop")
+			stack->pop();
+		else if (str == "size")
+			stack->size();
+		else if (str == "empty")
+			stack->empty();
+		else if (str == "top")
+			stack->top();
+		else if (str == "push") {
+			std::cin >> num;
+			stack->push(num);
+		}
+	}
+	int* a = new int(6);
+	std::cout << "ddd";
+	return 0;
+}
