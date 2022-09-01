a8a7cfbad822879522e7a7ef01bc413a061902c0
Gonagi <kkh10002@naver.com>
2022-09-01 16:31:10 +0900

    09.01

--- BaekJoon/203_Data_structure.cpp
+++ BaekJoon/203_Data_structure.cpp
@@ -1,5 +1,6 @@
 // 1935번 문제
 
+/*
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -11,7 +12,7 @@ double calculate(std::stack<int>& operator_stack, double& num1, double& num2);
 int main() {
 	char alphabet = 'A';
 	int N, count = 0;
-	double num,num1, num2, result;
+	double num, num1, num2, result;
 	std::stack<int> operator_stack;
 	std::stack<double> operand_stack;
 	std::vector<double> input;
@@ -25,14 +26,14 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		std::cin >> num;
 
-		
+
 		for (int j = 0; j < static_cast<int>(input.size()); j++) {
 			if (str.at(j) == alphabet)
 				input[j] = num;
 		}
 		alphabet++;
 	}
-	
+
 	for (int i = static_cast<int>(str.size()) - 1; i >= 0; i--) {
 		if (str.at(i) == '*' || str.at(i) == '/' ||
 			str.at(i) == '+' || str.at(i) == '-') {										// 연산자
@@ -92,4 +93,184 @@ double calculate(std::stack<int>& operator_stack, double& num1, double& num2) {
 	operator_stack.pop();
 
 	return result;
+}
+*/
+/*
+#include <iostream>
+#include <stack>
+#include <vector>
+
+int main() {
+	int N;
+	std::string str;
+	std::cin >> N >> str;
+
+	std::vector<int> vector(N);
+	std::stack<double> stack;
+
+	for (int i = 0; i < N; i++)
+		std::cin >> vector[i];
+
+	for (int i = 0; i < str.length(); i++) {
+		if ('A' <= str[i] && str[i] <= 'Z')	// 피연산자
+			stack.push(vector[str[i] - 'A']);
+		else {
+			if (!stack.empty()) {	// 연산자
+				double tmp = stack.top();
+				stack.pop();
+				if (str[i] == '+')
+					tmp = stack.top() + tmp;
+				else if (str[i] == '-')
+					tmp = stack.top() - tmp;
+				else if (str[i] == '*')
+					tmp = stack.top() * tmp;
+				else if (str[i] == '/')
+					tmp = stack.top() / tmp;
+				stack.pop();
+				stack.push(tmp);
+			}
+		}
+	}
+
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <stack>
+#include <vector>
+
+int prec(char ch);
+
+int main() {
+	std::string infix;
+	std::vector<char> postfix;
+	std::stack<char> stack;
+
+	std::cin >> infix;
+	
+	for (int i = 0; i < infix.size(); i++) {
+		if('A' <=infix.at(i) && infix.at(i) <= 'Z')	// 피연산자일때
+			postfix.push_back(infix.at(i));
+		else {	// 연산자일때
+			if (stack.empty())
+				stack.push(infix.at(i));
+			else if (prec(stack.top()) > prec(infix.at(i)))
+				stack.push(infix.at(i));
+			else {
+				while (!stack.empty() && prec(stack.top()) <= prec(infix.at(i))) {
+					postfix.push_back(stack.top());
+					stack.pop();
+				}
+				stack.push(infix.at(i));
+			}
+		}
+	}
+
+	while (!stack.empty()) {
+		postfix.push_back(stack.top());
+		stack.pop();
+	}
+
+	for (int i = 0; i < postfix.size(); i++)
+		std::cout << postfix.at(i);
+
+	return 0;
+}
+
+int prec(char ch) {
+	switch (ch) {
+	case '(' : case ')':
+		return 0;
+	case '*' : case '/':
+		return 1;
+	case '+' : case '-':
+		return 2;
+	}
+}
+*/
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+int prec(char ch);
+
+int main() {
+	std::string infix;
+	std::vector<char> postfix;
+	std::stack<char> stack;
+	int is_bracket = 0;
+
+	std::cin >> infix;
+
+	for (int i = 0; i < infix.size(); i++) {
+		if ('A' <= infix.at(i) && infix.at(i) <= 'Z')	// 피연산자일때
+			postfix.push_back(infix.at(i));
+		else {	// 연산자일때
+			if(is_bracket != 0){
+				if (infix.at(i) == '(')
+					is_bracket++;
+				if(stack.top() == '(')
+					stack.push(infix.at(i));
+				else {
+					if (infix.at(i) == ')') {
+						while (stack.top() != '(') {
+							postfix.push_back(stack.top());
+							stack.pop();
+						}
+						stack.pop();
+						is_bracket--;
+					}
+					else if (prec(stack.top()) > prec(infix.at(i)))
+						stack.push(infix.at(i));
+					else {
+						while (stack.top() != '(' && prec(stack.top()) <= prec(infix.at(i))) {
+							postfix.push_back(stack.top());
+							stack.pop();
+						}
+						stack.push(infix.at(i));
+					}
+				}
+			}
+
+			else {
+				if (infix.at(i) == '(')
+					is_bracket++;
+				if (stack.empty())
+					stack.push(infix.at(i));
+				else if (prec(stack.top()) > prec(infix.at(i)))
+					stack.push(infix.at(i));
+				else {
+					while (!stack.empty() && prec(stack.top()) <= prec(infix.at(i))) {
+						postfix.push_back(stack.top());
+						stack.pop();
+					}
+					stack.push(infix.at(i));
+				}
+			}
+		}
+	}
+
+	while (!stack.empty()) {
+		postfix.push_back(stack.top());
+		stack.pop();
+	}
+
+	for (int i = 0; i < postfix.size(); i++)
+		std::cout << postfix.at(i);
+
+	return 0;
+}
+
+int prec(char ch) {
+	switch (ch) {
+	case '(': case ')':
+		return 0;
+	case '*': case '/':
+		return 1;
+	case '+': case '-':
+		return 2;
+	}
 }
