b1096e6723633c8f58147bff5758208f3a0c5664
Gonagi <kkh10002@naver.com>
2022-09-29 21:48:29 +0900

    09.29 스티커 포도주 시식

--- BaekJoon/401_Dynamic.cpp
+++ BaekJoon/401_Dynamic.cpp
@@ -102,6 +102,7 @@ int main()
 }
 */
 
+/*
 // 11057번 문제
 
 #include <iostream>
@@ -110,6 +111,7 @@ int main()
 int main()
 {
     int N;
+    long long sum = 0;
     std::vector<long long> DP[10];
 
     DP[0].push_back(1);
@@ -127,19 +129,121 @@ int main()
 
     for (int i = 1; i < N; i++)
     {
-        DP[0].push_back((DP[0].at(i - 1) + DP[1].at(i - 1) + DP[2].at(i - 1) + DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[1].push_back((DP[1].at(i - 1) + DP[2].at(i - 1) + DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[2].push_back((DP[2].at(i - 1) + DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[3].push_back((DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[4].push_back((DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[5].push_back((DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[6].push_back((DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[7].push_back((DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[8].push_back((DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
-        DP[9].push_back((DP[9].at(i - 1)) % 10007 );
+        for(int index = 0; index < 10; index++){
+            for(int j = 0 + index; j < 10; j++)
+                sum += DP[j].at(i-1);
+            DP[index].push_back(sum % 10007);
+            sum = 0;
+        }
+        
+        // DP[0].push_back((DP[0].at(i - 1) + DP[1].at(i - 1) + DP[2].at(i - 1) + DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[1].push_back((DP[1].at(i - 1) + DP[2].at(i - 1) + DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[2].push_back((DP[2].at(i - 1) + DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[3].push_back((DP[3].at(i - 1) + DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[4].push_back((DP[4].at(i - 1) + DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[5].push_back((DP[5].at(i - 1) + DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[6].push_back((DP[6].at(i - 1) + DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[7].push_back((DP[7].at(i - 1) + DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[8].push_back((DP[8].at(i - 1) + DP[9].at(i - 1)) % 10007 );
+        // DP[9].push_back((DP[9].at(i - 1)) % 10007 );
+        
     }
 
     std::cout << (DP[0].at(N - 1) + DP[1].at(N - 1) + DP[2].at(N - 1) + DP[3].at(N - 1) + DP[4].at(N - 1) + DP[5].at(N - 1) + DP[6].at(N - 1) + DP[7].at(N - 1) + DP[8].at(N - 1) + DP[9].at(N - 1)) % 10007 << "\n";
+
+    return 0;
+}
+*/
+
+/*
+// 9465번 문제
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int T, num;
+    long long n;
+    std::vector<int> input[2], dp[2];
+    std::cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        std::cin >> n;
+
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                std::cin >> num;
+                input[j].push_back(num);
+            }
+        }
+
+        dp[0].push_back(input[0].at(0));
+        dp[1].push_back(input[1].at(0));
+
+        if (n > 1)
+        {
+            dp[0].push_back(input[0].at(1) + input[1].at(0));
+            dp[1].push_back(input[0].at(0) + input[1].at(1));
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[0].push_back(input[0].at(i) + std::max(dp[1].at(i - 1), dp[1].at(i - 2)));
+                dp[1].push_back(input[1].at(i) + std::max(dp[0].at(i - 1), dp[0].at(i - 2)));
+            }
+        }
+
+        std::cout << std::max(dp[0].at(n - 1), dp[1].at(n - 1)) << "\n";
+
+        for (int i = 0; i < 2; i++)
+        {
+            input[i].clear();
+            input[i].shrink_to_fit();
+            dp[i].clear();
+            dp[i].shrink_to_fit();
+        }
+    }
+    return 0;
+}
+*/
+
+// 2156번 문제
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+    int n, num;
+    std::vector<int> cup, dp;
+    std::cin >> n;
+
+    for(int i = 0; i < n; i++){
+        std::cin >> num;
+        cup.push_back(num);
+    }
+
+    dp.push_back(cup.at(0));
+    
+    if(n > 1){
+        dp.push_back(dp.at(0) + cup.at(1));
+
+        if(n > 2){
+            dp.push_back(std::max({cup.at(1) + cup.at(2), cup.at(0) + cup.at(2), cup.at(0) + cup.at(1)}));
+
+            for(int i = 3; i < n; i++){
+                dp.push_back(std::max({dp.at(i-3) + cup.at(i-1) + cup.at(i),
+                                        dp.at(i-2) + cup.at(i),
+                                        dp.at(i-1)}));
+            }
+        }
+    }
+
+    std::cout << dp.at(n-1);
 
     return 0;
 }
