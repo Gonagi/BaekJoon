c181fd950c52adbe9c912cfadc88b3601cef5b71
Gonagi <kkh10002@naver.com>
2022-10-13 16:56:32 +0900

    10.13 사탕 게임 날짜 계산

--- BaekJoon/500_bruteforce.cpp
+++ BaekJoon/500_bruteforce.cpp
@@ -35,47 +35,123 @@ int main(){
 }
 */
 
+/*
 // 3085번 문제
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
+
+int check_x(const std::vector<std::vector<char>>& vec, int N);
+int check_y(const std::vector<std::vector<char>>& vec, int N);
 
 int main(){
     int N, max = 0;
+    char ch;
+    std::vector<std::vector<char>> vec1;
+    std::vector<char> vec2;
+
     std::cin >> N;
 
-    std::string Board[N];
-    std::vector<std::vector<int>> Length(N, std::vector<int>(N, 1));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            std::cin >> ch;
+            vec2.push_back(ch);
+        }
+        vec1.push_back(vec2);
+        vec2.clear();
+        vec2.shrink_to_fit();
+    }
+
+    max = std::max({max, check_x(vec1, N), check_y(vec1, N)});
 
     for (int i = 0; i < N; i++)
-        std::cin >> Board[i];
-
-    for(int i = 0; i < N; i++)
-        for(int j = 1; j < N; j++)
-            if(Board[i].at(j-1) == Board[i].at(j))
-                max = std::max(max, Length[i].at(j)++);
-
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j <N; j++)
-            Length[i].at(j) = 1;
-
-    for (int j = 0; j < N; j++)
-        for (int i = 1; i < N; i++)
-            if (Board[i].at(j) == Board[i - 1].at(j))
-                max = std::max(max, Length[i].at(j)++);
-
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j <N; j++)
-            Length[i].at(j) = 1;
-
-    for(int i = 0; i < N; i++){
-        for(int j = 1; j < N; j++){
-            if(Board[i].at(j-1) != Board[i].at(j)){
-                if(j+1 < N && Board[i].at(j-1) == Board[i].at(j+1))
+    {
+        for (int j = 0; j < N - 1; j++)
+        {
+            if (vec1[i].at(j) != vec1[i].at(j + 1))
+            {
+                std::swap(vec1[i].at(j), vec1[i].at(j + 1));
+                max = std::max({max, check_x(vec1, N), check_y(vec1, N)});
+                std::swap(vec1[i].at(j), vec1[i].at(j + 1));
             }
         }
     }
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (vec1[i].at(j) != vec1[i+1].at(j))
+            {
+                std::swap(vec1[i].at(j), vec1[i+1].at(j));
+                max = std::max({max, check_x(vec1, N), check_y(vec1, N)});
+                std::swap(vec1[i].at(j), vec1[i+1].at(j));
+            }
+        }
+    }
+
+    std::cout << max << "\n";
+    return 0;
+}
+
+int check_x(const std::vector<std::vector<char>> &vec, int N)
+{
+    int max = 1, check = 1;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N - 1; j++)
+        {
+            if (vec[i].at(j) == vec[i].at(j + 1))
+                max = std::max(max, ++check);
+            else
+                check = 1;
+        }
+        check = 1;
+    }
+    return max;
+}
+
+int check_y(const std::vector<std::vector<char>> &vec, int N)
+{
+    int max = 1, check = 1;
+
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < N - 1; i++)
+        {
+            if (vec[i].at(j) == vec[i + 1].at(j))
+                max = std::max(max, ++check);
+
+            else
+                check = 1;
+        }
+        check = 1;
+    }
+
+    return max;
+}
+*/
+
+// 1476번 문제
+
+#include <iostream>
+
+int main()
+{
+    long long a, b, c, i = 1;
+    std::cin >> a >> b >> c;
+
+    while (1)
+    {
+        if (((i % 15 == a) || (i % 15 == a-15)) &&
+            ((i % 28 == b) || (i % 28 == b-28)) &&
+            ((i % 19 == c) || (i % 19 == c-19)))
+            break;
+        i++;
+    }
+
+    std::cout << i << "\n";
     return 0;
 }
