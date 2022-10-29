1df40a05788cd750dd0c415e945c5aee45388394
Gonagi <kkh10002@naver.com>
2022-10-29 18:03:31 +0900

    10.29 N과 M (3) N과 M (4) N과 M (5) N과 M (6)

--- BaekJoon/510_bruteforce.cpp
+++ BaekJoon/510_bruteforce.cpp
@@ -40,6 +40,8 @@ void print(int count)
     }
 }
 */
+
+/*
 // 15650번 문제
 
 #include <iostream>
@@ -78,4 +80,167 @@ void print(int count, int cur)
             visited.at(index) = false;
         }
     }
+}
+*/
+
+/*
+// 11651번 문제
+
+#include <iostream>
+#include <vector>
+
+int M, N;
+std::vector<int> vec(8,0);
+void print(int check);
+
+int main(){
+    std::cin >> N >> M;
+
+    print(0);
+    return 0;
+}
+
+void print(int check){
+    if(check == M){
+        for(int index = 1; index <= M; index++)
+            std::cout << vec.at(index)<< " ";
+        std::cout << "\n";
+
+        return;
+    }
+
+    for(int num = 1; num <= N; num++){
+        vec.at(check+1) = num;
+        print(check+1);
+    }
+}
+*/
+
+/*
+// 11652번 문제
+
+#include <iostream>
+#include <vector>
+
+int N, M;
+std::vector<int> vec(9,0);
+
+void print(int check, int cur_num);
+
+int main(){
+    std::cin >> N >> M;
+
+    print(0, 1);
+    return 0;
+}
+
+void print(int check, int cur_num){
+    if(check == M){
+        for(int index = 1; index <= M; index++)
+            std::cout << vec.at(index) << " ";
+        std::cout <<"\n";
+
+        return;
+    }
+
+    for(int num = cur_num; num <= N; num++){
+        vec.at(check+1) = num;
+        print(check+1, num);
+    }
+}
+*/
+
+/*
+// 15654번 문제
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int N, M, num;
+std::vector<int> input, vec(8, 0);
+std::vector<bool> visited(8, false);
+
+void DFS(int count);
+
+int main()
+{
+    std::cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        std::cin >> num;
+        input.push_back(num);
+    }
+
+    std::sort(input.begin(), input.end());
+    DFS(0);
+
+    return 0;
+}
+
+void DFS(int count)
+{
+    if (count == M)
+    {
+        for (int index = 0; index < M; index++)
+            std::cout << vec.at(index) << " ";
+        std::cout << "\n";
+    }
+
+    for (int num = 0; num < N; num++)
+    {
+        if (!visited.at(num))
+        {
+            vec.at(count) = input.at(num);
+            visited.at(num) = true;
+            DFS(count + 1);
+            visited.at(num) = false;
+        }
+    }
+}
+*/
+
+// 15655번 문제
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int N, M;
+std::vector<int> input, vec(8);
+std::vector<bool> visited(8, false);
+
+void DFS(int count, int cur_num);
+
+int main() {
+  int num;
+  std::cin >> N >> M;
+
+  for (int i = 0; i < N; i++) {
+    std::cin >> num;
+    input.push_back(num);
+  }
+
+  std::sort(input.begin(), input.end());
+  DFS(0, 0);
+
+  return 0;
+}
+
+void DFS(int count, int cur_num) {
+  if (count == M) {
+    for (int index = 0; index < M; index++)
+      std::cout << vec.at(index) << " ";
+    std::cout << "\n";
+  }
+
+  for (int num = cur_num; num < N; num++) {
+    if (!visited.at(num)) {
+      vec.at(count) = input.at(num);
+      visited.at(num) = true;
+      DFS(count + 1, num+1);
+      //visited.at(num) = false;
+    }
+  }
 }
