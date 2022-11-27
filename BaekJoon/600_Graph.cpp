b120a20f90c49ab3b1c8e48a609fa89e926200a8
Gonagi <kkh10002@naver.com>
2022-11-27 21:12:30 +0900

    연결 요소의 개수(11724)

--- BaekJoon/600_Graph.cpp
+++ BaekJoon/600_Graph.cpp
@@ -5,45 +5,46 @@
 #include <iostream>
 #include <vector>
 
-int N, M, is_exist = 0;
-bool visited[2000];
-std::vector<int> vec[2000];
+int N, M;
+bool is_exist = false;
+std::vector<int> linked[2001];
+bool visited[2001];
 
 void DFS(int cur, int depth);
 
 int main() {
-    int from, to;
+    int a, b;
     std::cin >> N >> M;
 
     for (int i = 0; i < M; i++) {
-        std::cin >> from >> to;
-        vec[from].push_back(to);
-        vec[to].push_back(from);
+        std::cin >> a >> b;
+        linked[a].push_back(b);
+        linked[b].push_back(a);
     }
 
-    int i = 0;
-    while (i < N && !is_exist) {
-        DFS(i, 0);
-        i++;
-    }
+    for (int index = 0; index < N; index++)
+        DFS(index, 0);
 
-    std::cout << is_exist << "\n";
+    if (!is_exist)
+        std::cout << 0 << "\n";
 
     return 0;
 }
 
 void DFS(int cur, int depth) {
     if (depth == 4) {
-        is_exist = 1;
+        std::cout << 1 << "\n";
+        is_exist = true;
         return;
     }
 
-    for (int index = 0; index < vec[cur].size(); index++) {
-        visited[cur] = true;
-        int next = vec[cur].at(index);
-        if (!visited[next] && !is_exist)
+    for (int index = 0; index < linked[cur].size(); index++) {
+        int next = linked[cur].at(index);
+        if (!visited[next] && !is_exist) {
+            visited[cur] = true;
             DFS(next, depth + 1);
-        visited[cur] = false;
+            visited[cur] = false;
+        }
     }
 }
 */
@@ -96,6 +97,7 @@ void DFS(int cur, int depth) {
 }
 */
 
+/*
 // DFS와 BFS
 // 1260
 
@@ -145,30 +147,6 @@ void DFS(int cur) {
     return;
 }
 
-/*
-void BFS(int cur, int count) {
-    if (count != 0 && que.empty())
-        return;
-
-    std::cout << cur << " ";
-    visited[cur] = true;
-
-    for (int next_index = 1; next_index <= N; next_index++) {
-        if (vec[cur][next_index] == 1 && !visited[next_index]) {
-            que.push(next_index);
-            visited[next_index] = true;
-            count++;
-        }
-    }
-
-    int next = que.front();
-    que.pop();
-    if (que.empty())
-        std::cout << next << " ";
-    BFS(next, count);
-}
-*/
-
 void BFS(int cur, int count) {
     que.push(cur);
     visited[cur] = true;
@@ -184,6 +162,125 @@ void BFS(int cur, int count) {
                 visited[next_index] = true;
                 std::cout << next_index << " ";
             }
+        }
+    }
+}
+*/
+
+/*
+#include <iostream>
+#include <queue>
+#include <vector>
+
+int N, M, V;
+int linked[1001][1001];
+bool visited[1001];
+std::queue<int> que;
+
+void DFS(int cur, int count);
+void BFS(int cur, int count);
+
+int main() {
+    int a, b;
+    std::cin >> N >> M >> V;
+
+    for (int i = 0; i < M; i++) {
+        std::cin >> a >> b;
+        linked[a][b]++;
+        linked[b][a]++;
+    }
+
+    DFS(V, 0);
+
+    std::cout << "\n";
+    for (int i = 1; i <= N; i++)
+        visited[i] = false;
+
+    BFS(V, 0);
+
+    return 0;
+}
+
+void DFS(int cur, int count) {
+    visited[cur] = true;
+    std::cout << cur << " ";
+
+    for (int index = 1; index <= N; index++)
+        if (!visited[index] && linked[cur][index] > 0)
+            DFS(index, count + 1);
+}
+
+void BFS(int cur, int count) {
+    que.push(cur);
+    std::cout << cur << " ";
+    visited[cur] = true;
+
+    while (!que.empty()) {
+        int from = que.front();
+        que.pop();
+
+        for (int to = 1; to <= N; to++) {
+            if (!visited[to] && linked[from][to] > 0 && linked[to][from] > 0) {
+                linked[from][to]--;
+                linked[to][from]--;
+
+                std::cout << to << " ";
+                que.push(to);
+                visited[to] = true;
+            }
+        }
+    }
+}
+*/
+
+// 연결 요소의 개수
+// 11724
+
+#include <iostream>
+#include <vector>
+
+int N, M, count = 0;
+bool linked[1001][1001];
+bool visited[1001];
+
+void input();
+void count_link();
+void DFS(int num);
+
+int main() {
+    input();
+    count_link();
+    std::cout << count << "\n";
+
+    return 0;
+}
+
+void input() {
+    int u, v;
+    std::cin >> N >> M;
+
+    for (int i = 1; i <= M; i++) {
+        std::cin >> u >> v;
+        linked[u][v] = true;
+        linked[v][u] = true;
+    }
+}
+
+void count_link() {
+    for (int num = 1; num <= N; num++) {
+        if (!visited[num]) {
+            visited[num] = true;
+            DFS(num);
+            count++;
+        }
+    }
+}
+
+void DFS(int num) {
+    for (int to = 1; to <= N; to++) {
+        if (!visited[to] && linked[num][to]) {
+            visited[to] = true;
+            DFS(to);
         }
     }
 }
