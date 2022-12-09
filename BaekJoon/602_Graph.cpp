998473fefc4caa71437135abea70d6317e3575e8
Gonagi <kkh10002@naver.com>
2022-12-09 15:55:53 +0900

    DFS 스페셜 저지(16964)

--- BaekJoon/602_Graph.cpp
+++ BaekJoon/602_Graph.cpp
@@ -174,3 +174,140 @@ void BFS() {
         std::cout << 0 << "\n";
 }
 */
+
+/*
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+int N, order[100001];
+std::vector<int> linked[100001];
+std::queue<int> result;
+
+bool comp(const int &a, const int &b) {
+    return order[a] < order[b];
+}
+void input();
+void BFS();
+
+int main() {
+    input();
+    BFS();
+    return 0;
+}
+
+void input() {
+    int dot1, dot2, num;
+    std::cin >> N;
+
+    for (int i = 0; i < N - 1; i++) {
+        std::cin >> dot1 >> dot2;
+        linked[dot1].push_back(dot2);
+        linked[dot2].push_back(dot1);
+    }
+
+    for (int i = 0; i < N; i++) {
+        std::cin >> num;
+        result.push(num);
+        order[num] = i;
+    }
+
+    for (int i = 1; i <= N; i++)
+        std::stable_sort(linked[i].begin(), linked[i].end(), comp);
+}
+
+void BFS() {
+    if (result.front() != 1) {
+        std::cout << 0 << "\n";
+        exit(0);
+    }
+
+    std::queue<int> que;
+    que.push(1);
+    result.pop();
+
+    while (!que.empty()) {
+        int cur_num = que.front();
+        que.pop();
+
+        for (auto next_num : linked[cur_num]) {
+            if (next_num == result.front()) {
+                que.push(next_num);
+                result.pop();
+            }
+        }
+    }
+
+    if (result.empty())
+        std::cout << 1 << "\n";
+    else
+        std::cout << 0 << "\n";
+}
+*/
+
+// DFS 스페셜 저지
+// 16964
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int N, count, order[100001];
+bool check[100001];
+std::vector<int> linked[100001], result;
+
+void input();
+void DFS(int cur_num);
+bool comp(const int &a, const int &b) {
+    return order[a] < order[b];
+}
+
+int main() {
+    input();
+    if (result[0] != 1) {
+        std::cout << 0 << "\n";
+        exit(0);
+    }
+    DFS(result[0]);
+    std::cout << 1 << "\n";
+    return 0;
+}
+
+void input() {
+    int dot1, dot2, num;
+
+    std::cin >> N;
+    for (int i = 0; i < N - 1; i++) {
+        std::cin >> dot1 >> dot2;
+        linked[dot1].push_back(dot2);
+        linked[dot2].push_back(dot1);
+    }
+
+    for (int i = 0; i < N; i++) {
+        std::cin >> num;
+        result.push_back(num);
+        order[num] = i;
+    }
+
+    for (int i = 1; i <= N; i++)
+        std::stable_sort(linked[i].begin(), linked[i].end(), comp);
+}
+
+void DFS(int cur_num) {
+    if (check[cur_num]) {
+        return;
+    }
+
+    check[cur_num] = true;
+
+    for (int next_num : linked[cur_num])
+        if (!check[next_num]) {
+            count++;
+            if (next_num != result[count]) {
+                std::cout << 0 << "\n";
+                exit(0);
+            }
+            DFS(next_num);
+        }
+}
