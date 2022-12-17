135ca0ac05f18e7a9ad863b680e2ca38f6873146
Gonagi <kkh10002@naver.com>
2022-12-17 23:09:09 +0900

    숨바꼭질4(3913)

--- BaekJoon/610_BFS.cpp
+++ BaekJoon/610_BFS.cpp
@@ -1,3 +1,4 @@
+/*
 // 숨바꼭질
 // 1697
 
@@ -48,6 +49,73 @@ void BFS() {
         if (num <= 50000 && !visited[2 * num]) {
             visited[2 * num] = true;
             que.push(std::make_pair((2 * num), count + 1));
+        }
+    }
+}
+*/
+
+// 숨바꼭질 4
+// 13913
+
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <vector>
+
+int N, K, num[100001];
+std::vector<bool> visited;
+std::vector<int> result;
+
+void BFS();
+
+int main() {
+    std::cin >> N >> K;
+    visited.assign(100001, false);
+
+    BFS();
+
+    return 0;
+}
+
+void BFS() {
+    std::queue<std::pair<int, int>> que;
+    que.push(std::make_pair(N, 0));
+    visited.at(N) = true;
+
+    while (!que.empty()) {
+        int cur = que.front().first;
+        int count = que.front().second;
+        que.pop();
+
+        if (cur == K) {
+            int index = cur;
+            while (index != N) {
+                result.push_back(num[index]);
+                index = num[index];
+            }
+
+            std::cout << count << "\n";
+
+            for (int i = count - 1; i >= 0; i--)
+                std::cout << result.at(i) << " ";
+            std::cout << K << "\n";
+            return;
+        }
+
+        if (0 < cur && !visited[cur - 1]) {
+            que.push(std::make_pair(cur - 1, count + 1));
+            visited[cur - 1] = true;
+            num[cur - 1] = cur;
+        }
+        if (cur < 100000 && !visited[cur + 1]) {
+            que.push(std::make_pair(cur + 1, count + 1));
+            visited[cur + 1] = true;
+            num[cur + 1] = cur;
+        }
+        if (cur <= 50000 && !visited[2 * cur]) {
+            que.push(std::make_pair(2 * cur, count + 1));
+            visited[2 * cur] = true;
+            num[2 * cur] = cur;
         }
     }
 }
