2fe8ccacad95f6f77b92c7f1fdb7ca1b6b4c12be
Gonagi <kkh10002@naver.com>
2022-12-22 16:00:07 +0900

    알고스팟(1261)

--- BaekJoon/610_BFS.cpp
+++ BaekJoon/610_BFS.cpp
@@ -246,6 +246,9 @@ void BFS() {
 }
 */
 
+/*
+// 우선순위 큐
+
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -295,3 +298,185 @@ void min_second() {
         }
     }
 }
+*/
+
+// 알고스팟
+// 1261
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+#define Max 100 + 1
+
+struct Direction {
+    int y, x;
+};
+
+Direction dir[4]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+int N, M;
+bool visited[Max][Max];
+std::vector<std::vector<int>> map, count;
+
+void input();
+void BFS();
+bool is_movable(int y, int x);
+
+int main() {
+    input();
+
+    BFS();
+
+    std::cout << count[N - 1][M - 1] << "\n";
+
+    return 0;
+}
+
+void input() {
+    std::string str;
+    std::vector<int> str_x, count_x;
+
+    std::cin >> M >> N;
+
+    count_x.assign(M, Max * Max + 1);
+
+    for (int y = 0; y < N; y++) {
+        std::cin >> str;
+
+        for (int x = 0; x < M; x++)
+            str_x.push_back(str[x] - '0');
+
+        map.push_back(str_x);
+        count.assign(N, count_x);
+
+        str_x.clear();
+        str_x.shrink_to_fit();
+    }
+}
+
+void BFS() {
+    std::queue<Direction> que;
+    que.push({0, 0});
+    count[0][0] = 0;
+
+    while (!que.empty()) {
+        int cur_y = que.front().y;
+        int cur_x = que.front().x;
+
+        que.pop();
+
+        for (int d = 0; d < 4; d++) {
+            int next_y = cur_y + dir[d].y;
+            int next_x = cur_x + dir[d].x;
+
+            if (is_movable(next_y, next_x)) {
+                if (map[next_y][next_x] == 1) {
+                    if (count[next_y][next_x] > count[cur_y][cur_x] + 1) {
+                        count[next_y][next_x] = count[cur_y][cur_x] + 1;
+                        que.push({next_y, next_x});
+                    }
+                } else if (map[next_y][next_x] == 0) {
+                    if (count[next_y][next_x] > count[cur_y][cur_x]) {
+                        count[next_y][next_x] = count[cur_y][cur_x];
+                        que.push({next_y, next_x});
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool is_movable(int y, int x) {
+    if (0 <= y && y <= N - 1 && 0 <= x && x <= M - 1)
+        return true;
+    return false;
+}
+
+/*
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+#define Max 100 + 1
+
+struct Direction {
+    int y, x, count;
+};
+struct cmp {
+    bool operator()(Direction a, Direction b) {
+        return a.count > b.count;
+    }
+};
+Direction dir[4]{{-1, 0, 0}, {0, 1, 0}, {1, 0, 0}, {0, -1, 0}};
+int N, M, visited[Max][Max];
+std::vector<std::vector<int>> map;
+
+void input();
+void BFS();
+bool is_movable(int y, int x);
+
+int main() {
+    input();
+
+    BFS();
+
+    return 0;
+}
+
+void input() {
+    std::string str;
+    std::vector<int> str_x;
+
+    std::cin >> M >> N;
+
+    for (int y = 0; y < N; y++) {
+        std::cin >> str;
+
+        for (int x = 0; x < M; x++)
+            str_x.push_back(str[x] - '0');
+
+        map.push_back(str_x);
+
+        str_x.clear();
+        str_x.shrink_to_fit();
+    }
+}
+
+void BFS() {
+    std::priority_queue<Direction, std::vector<Direction>, cmp> que;
+    que.push({0, 0, 0});
+
+    while (!que.empty()) {
+        int cur_y = que.top().y;
+        int cur_x = que.top().x;
+        int cur_count = que.top().count;
+        visited[cur_y][cur_x] = true;
+        que.pop();
+
+        if (cur_y == N - 1 && cur_x == M - 1) {
+            std::cout << cur_count << "\n";
+            return;
+        }
+
+        for (int d = 0; d < 4; d++) {
+            int next_y = cur_y + dir[d].y;
+            int next_x = cur_x + dir[d].x;
+
+            if (is_movable(next_y, next_x) && !visited[next_y][next_x]) {
+                if (map[next_y][next_x] == 1)
+                    que.push({next_y, next_x, cur_count + 1});
+                else
+                    que.push({next_y, next_x, cur_count});
+            }
+        }
+    }
+}
+
+bool is_movable(int y, int x) {
+    if (0 <= y && y <= N - 1 && 0 <= x && x <= M - 1)
+        return true;
+    return false;
+}
+*/
