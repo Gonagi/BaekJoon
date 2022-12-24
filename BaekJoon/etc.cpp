f41983077d37bdee2f65f5375cd477b1d6845756
Gonagi <kkh10002@naver.com>
2022-12-24 14:15:14 +0900

    별 찍기 - 11(2448)

--- BaekJoon/etc.cpp
+++ BaekJoon/etc.cpp
@@ -133,6 +133,7 @@ int main() {
 }
 */
 
+/*
 // 계단 오르기
 // 2579
 
@@ -166,4 +167,48 @@ int main() {
     std::cout << dp[top - 1] << "\n";
 
     return 0;
+}
+*/
+
+// 별찍기 -11
+// 2448
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char star[3072][6144];//세로 N, 가로 2N-1
+
+void print(int x, int y, int n) {
+	if (n == 3) {
+		star[x][y] = '*';
+		star[x + 1][y - 1] = '*';
+		star[x + 2][y - 2] = '*';
+		star[x + 2][y - 1] = '*';
+		star[x + 2][y] = '*';
+		star[x + 2][y + 1] = '*';
+		star[x + 2][y + 2] = '*';
+		star[x + 1][y + 1] = '*';
+	}
+	else {
+		print(x, y, n / 2);
+		print(x + n / 2, y - n / 2, n / 2);
+		print(x + n / 2, y + n / 2, n / 2);
+	}
+}
+
+int main() {
+	int N;
+	cin >> N;
+	memset(star, ' ', sizeof(star));
+
+	print(0, N - 1, N);
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < 2 * N - 1; j++) {
+			cout << star[i][j];
+		}
+		cout << '\n';
+	}
+	return 0;
 }
