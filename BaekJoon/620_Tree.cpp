751cb4d6317a75958fbc9c774d3e8fabf07bd2e2
Gonagi <kkh10002@naver.com>
2022-12-25 15:14:50 +0900

    트리의 높이와 너비(2250)

--- BaekJoon/620_Tree.cpp
+++ BaekJoon/620_Tree.cpp
@@ -1,3 +1,6 @@
+/*
+// 트리 순회
+// 1991
 #include <iostream>
 
 int N;
@@ -48,4 +51,71 @@ void postorder(char ch) {
         postorder(arr[(ch - 'A')].second);
         std::cout << ch;
     }
+}
+*/
+
+// 트리의 높이와 너비
+// 2250
+
+#include <algorithm>
+#include <iostream>
+#define Max 100000 + 1
+
+void inorder(int num);
+void find_min();
+
+int N, level, idx = 1, count[Max], low[Max], high[Max];
+std::pair<int, int> arr[Max];
+
+int main() {
+    std::fill_n(low, Max, Max);
+    int node, left, right;
+
+    std::cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        std::cin >> node >> left >> right;
+        arr[node].first = left;
+        arr[node].second = right;
+
+        count[node]++;
+        count[left]++;
+        count[right]++;
+    }
+
+    int root;
+    for (int i = 1; i <= N; i++) {
+        if (count[i] == 1) {
+            root = i;
+            break;
+        }
+    }
+
+    inorder(root);
+    find_min();
+    return 0;
+}
+
+void inorder(int num) {
+    if (num != -1) {
+        level++;
+        inorder(arr[num].first);
+        low[level] = std::min(low[level], idx);
+        high[level] = std::max(high[level], idx++);
+        inorder(arr[num].second);
+        level--;
+    }
+}
+
+void find_min() {
+    int max_level = 1, max = 0;
+    for (int cur_level = 1; cur_level <= N; cur_level++) {
+        if (max < high[cur_level] - low[cur_level] + 1) {
+            max = high[cur_level] - low[cur_level] + 1;
+            max_level = cur_level;
+        }
+    }
+
+    std::cout << max_level << " " << max << "\n";
+    return;
 }
