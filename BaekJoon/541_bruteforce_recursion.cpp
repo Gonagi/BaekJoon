c6eada70bd61fc799ccbaf0f02a099f9832851e8
Gonagi <kkh10002@naver.com>
2023-01-14 15:33:48 +0900

    가르침(1062)

--- BaekJoon/541_bruteforce_recursion.cpp
+++ BaekJoon/541_bruteforce_recursion.cpp
@@ -1,3 +1,7 @@
+/*
+// 부분수열의 합
+// 14225
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -31,4 +35,147 @@ int main() {
         }
     }
     return 0;
+}
+*/
+
+/*
+// 가르침
+// 1062
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int N, K, check, max;
+bool alphabet[26];
+int word[50];
+std::vector<int> vec;
+
+void input();
+void DFS(int pick, int start, int checked);
+
+int main() {
+    input();
+
+    if (K < 5)
+        std::cout << 0 << "\n";
+    else if (K == 26)
+        std::cout << N << "\n";
+    else {
+        check |= 1 << ('a' - 'a');
+        check |= 1 << ('n' - 'a');
+        check |= 1 << ('t' - 'a');
+        check |= 1 << ('i' - 'a');
+        check |= 1 << ('c' - 'a');
+        DFS(K - 5, 0, check);
+        std::cout << max << "\n";
+    }
+    return 0;
+}
+
+void input() {
+    std::string str;
+
+    std::cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        int num = 0;
+        std::cin >> str;
+        for (int idx = 0; idx < str.length(); idx++) {
+            num |= 1 << (str[idx] - 'a');
+        }
+        word[i] = num;
+    }
+}
+
+void DFS(int pick, int start, int checked) {
+    if (pick == 0) {
+        int count = 0;
+        for (int idx = 0; idx < N; idx++) {
+            if ((word[idx] & checked) == word[idx])
+                count++;
+        }
+        if (max < count)
+            max = count;
+        return;
+    }
+
+    for (int idx = start; idx < 26; idx++) {
+        if ((checked & (1 << idx)) == 0) {
+            checked |= (1 << idx);
+            DFS(pick - 1, idx, checked);
+            checked &= ~(1 << idx);
+        }
+    }
+}
+*/
+
+#include <iostream>
+#include <string>
+
+int N, K, max, word[50];
+
+void input();
+void DFS(int count, int cur, int check);
+
+int main() {
+    input();
+
+    if (K < 5)
+        std::cout << 0 << "\n";
+    else if (K == 26)
+        std::cout << N << "\n";
+    else {
+        int check = 0;
+        check |= (1 << ('a' - 'a'));
+        check |= (1 << ('c' - 'a'));
+        check |= (1 << ('i' - 'a'));
+        check |= (1 << ('n' - 'a'));
+        check |= (1 << ('t' - 'a'));
+        DFS(5, 0, check);
+        std::cout << max << "\n";
+    }
+    return 0;
+}
+
+void input() {
+    std::string str;
+    std::cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        int check = 0;
+        check |= (1 << ('a' - 'a'));
+        check |= (1 << ('c' - 'a'));
+        check |= (1 << ('i' - 'a'));
+        check |= (1 << ('n' - 'a'));
+        check |= (1 << ('t' - 'a'));
+
+        std::cin >> str;
+        for (int idx = 0; idx < str.length(); idx++)
+            if ((check & (1 << (str[idx] - 'a'))) == 0)
+                check |= (1 << (str[idx] - 'a'));
+
+        word[i] = check;
+    }
+}
+
+void DFS(int count, int cur, int check) {
+    if (count == K) {
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if ((word[i] & check) == word[i])
+                count++;
+        }
+        if (max < count)
+            max = count;
+        return;
+    }
+
+    for (int idx = cur; idx < 26; idx++) {
+        if ((check & (1 << idx)) == 0) {
+            check |= (1 << idx);
+            DFS(count + 1, idx, check);
+            check &= ~(1 << idx);
+        }
+    }
 }
