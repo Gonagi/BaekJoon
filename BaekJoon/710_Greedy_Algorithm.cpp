b7fdc538748022a0ec157fd632e5a1fa4af938af
Gonagi <kkh10002@naver.com>
2023-02-26 16:10:04 +0900

    회의실 배정(1931)

--- BaekJoon/710_Greedy_Algorithm.cpp
+++ BaekJoon/710_Greedy_Algorithm.cpp
@@ -1,3 +1,4 @@
+/*
 // 동전 0
 // 11047
 
@@ -33,4 +34,51 @@ int main() {
     std::cout << result << '\n';
 
     return 0;
+}
+*/
+
+// 회의실 배정
+// 1931
+
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+int N;
+std::vector<std::pair<int, int>> vec;
+
+bool comp(const std::pair<int, int> &a, const std::pair<int, int> &b);
+
+int main() {
+    int start, end;
+
+    std::cin >> N;
+
+    for (int idx = 0; idx < N; idx++) {
+        std::cin >> start >> end;
+        vec.push_back({start, end});
+    }
+
+    std::sort(vec.begin(), vec.end(), comp);
+
+    end = vec[0].second;
+    int count = 1;
+
+    for (int idx = 1; idx < N; idx++) {
+        if (end <= vec[idx].first) {
+            count++;
+            end = vec[idx].second;
+        }
+    }
+
+    std::cout << count << '\n';
+
+    return 0;
+}
+
+bool comp(const std::pair<int, int> &a, const std::pair<int, int> &b) {
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
 }
