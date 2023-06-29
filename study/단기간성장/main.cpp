2649a4584fd25a8f3b32becff468f86bf1a19f21
Gonagi <kkh10002@naver.com>
2023-06-29 22:41:40 +0900

    이항 계수3(11401)

--- study/단기간성장/main.cpp
+++ study/단기간성장/main.cpp
@@ -144,6 +144,7 @@ int main()
 // 3197
 // 백조의 호수
 
+/*
 #include <iostream>
 #include <queue>
 #include <string>
@@ -293,4 +294,44 @@ bool check(Direction cur)
   }
 
   return false;
+}
+*/
+
+// 11401
+// 이항 계수 3
+
+#include <iostream>
+#define MOD 1000000007
+
+int main()
+{
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  long long result = 1, N, K;
+
+  std::cin >> N >> K;
+
+  for (long long num = K + 1; num <= N; num++)
+    result = (result * num) % MOD;
+
+  long long base = 1;
+  for (long long num = 1; num <= N - K; num++)
+    base = (base * num) % MOD;
+
+  long long exponent = MOD - 2;
+
+  while (exponent)
+  {
+    if (exponent & 1) // 홀수일때 (exponent % 2 == 1)
+      result = (result * base) % MOD;
+
+    base = (base * base) % MOD;
+    exponent = (exponent >> 1); // exponent /= 2
+  }
+
+  std::cout << result << '\n';
+
+  return 0;
 }
