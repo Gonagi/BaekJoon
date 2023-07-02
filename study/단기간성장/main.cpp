8a4673f95bf9324d45e4627335286878ec38965a
Gonagi <kkh10002@naver.com>
2023-07-03 02:10:52 +0900

    행렬 제곱(10830)

--- study/단기간성장/main.cpp
+++ study/단기간성장/main.cpp
@@ -336,6 +336,7 @@ int main()
 }
 */
 
+/*
 // 4344
 // 평균은 넘겠지
 #include <cmath>
@@ -372,9 +373,93 @@ int main()
     std::cout << std::fixed;
     std::cout.precision(3);
 
-    std::cout << std::round(((count) / N * 1000)) / 10 << "%\n";
+    std::cout << std::round((count) / N * 1000) / 10 << "%\n";
     vec.clear();
     vec.shrink_to_fit();
   }
+  return 0;
+}
+*/
+
+// 행렬 제곱
+// 10830
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  std::vector<std::vector<int>> vec;
+  std::vector<int> vec_x;
+  int N, num;
+  long long B;
+
+  std::cin >> N >> B;
+  std::vector<std::vector<int>> result(N, std::vector<int>(N, 0));
+
+  for (int y = 0; y < N; y++)
+  {
+    for (int x = 0; x < N; x++)
+    {
+      std::cin >> num;
+      vec_x.push_back(num);
+      if (y == x)
+        result[y][x] = 1;
+    }
+    vec.push_back(vec_x);
+
+    vec_x.clear();
+    vec_x.shrink_to_fit();
+  }
+
+  if (B != 1)
+  {
+    while (B > 0)
+    {
+      if (B & 1)
+      {
+        std::vector<std::vector<int>> temp(N, std::vector<int>(N, 0));
+
+        for (int k = 0; k < N; k++)
+        {
+          for (int y = 0; y < N; y++)
+          {
+            int cal = result[y][k];
+
+            for (int x = 0; x < N; x++)
+              temp[y][x] += (cal * vec[k][x]) % 1000;
+          }
+        }
+        result = temp;
+      }
+
+      std::vector<std::vector<int>> temp(N, std::vector<int>(N, 0));
+      for (int k = 0; k < N; k++)
+      {
+        for (int y = 0; y < N; y++)
+        {
+          int cal = vec[y][k];
+          for (int x = 0; x < N; x++)
+            temp[y][x] += (cal * vec[k][x]) % 1000;
+        }
+      }
+
+      vec = temp;
+
+      B = B >> 1;
+    }
+  }
+  else
+    result = vec;
+
+  for (int y = 0; y < N; y++)
+  {
+    for (int x = 0; x < N; x++)
+    {
+      std::cout << result[y][x] % 1000 << ' ';
+    }
+    std::cout << '\n';
+  }
+
   return 0;
 }
